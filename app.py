3027223ad379dc7919861df927d1726a2ef18d25
Allen-wa-ng <wang0911309998@gmail.com>
2021-10-25 23:54:21 +0800

    Added PostgreSQL environment variable adn phycopg2

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import os
+import re
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
 
 from flask import Flask
 from flask_restful import Api
